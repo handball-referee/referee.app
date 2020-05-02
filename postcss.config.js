1a3d4501a8fbff8ef922e299b682249cdfb939a0
Christoph Kraemer <mail@christophkraemer.de>
2020-05-02 09:12:22 +0200

    chore: optimize css definitions

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
+    "postcss-nesting": {},
+    "postcss-custom-properties": {
+      importFrom: "src/components/shared.css",
+    },
     "postcss-preset-env": {},
   },
 };
