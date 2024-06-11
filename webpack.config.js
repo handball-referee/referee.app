875ebfa16121e3afa74198b41d7ea67502ed7f3f
Christoph Kraemer <mail@christophkraemer.de>
2024-06-11 10:41:51 +0200

    fix: icon styling

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,43 @@ markdownRenderer.heading = function (text, level, raw, slugger) {
   return `<h${level} id="${escapedText}">${text}</h${level}>`;
 };
 
+const plugins = [
+  new EnvironmentPlugin(['SENTRY_DSN', 'SENTRY_ENV', 'GA_TRACKING_ID']),
+  new HtmlWebpackPlugin({
+    template: "index.html",
+  }),
+  new CopyPlugin({
+    patterns: [
+      {
+        from: "data/questions/*",
+      },
+      {
+        from: "static/*",
+      },
+      {
+        from: "favicon.ico",
+      },
+    ]
+  }),
+  new LoadablePlugin(),
+  /* new BundleAnalyzerPlugin({
+    analyzerMode: "static",
+    generateStatsFile: true,
+  }) */
+]
+
+if (process.env.NODE_ENV !== 'dev') {
+  plugins.push(
+    new GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      cacheId: "handball",
+      exclude: ["CNAME"],
+      offlineGoogleAnalytics: true,
+    })
+  );
+}
+
 const config = {
   context: appPath,
   mode: process.env.NODE_ENV === 'dev' ? 'development' : 'production',
@@ -86,37 +123,7 @@ const config = {
       chunks: "all",
     },
   },
-  plugins: [
-    new EnvironmentPlugin(['SENTRY_DSN', 'SENTRY_ENV', 'GA_TRACKING_ID']),
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "data/questions/*",
-        },
-        {
-          from: "static/*",
-        },
-        {
-          from: "favicon.ico",
-        },
-      ]
-    }),
-    new GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-      cacheId: "handball",
-      exclude: ["CNAME"],
-      offlineGoogleAnalytics: true,
-    }),
-    new LoadablePlugin(),
-    /* new BundleAnalyzerPlugin({
-      analyzerMode: "static",
-      generateStatsFile: true,
-    }) */
-  ],
+  plugins,
   devServer: {
     historyApiFallback: true,
     host: '0.0.0.0'
